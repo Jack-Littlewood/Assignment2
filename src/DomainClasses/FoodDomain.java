637b9f4a30bd7835153937d65104c6423b588be2
Jack <jackadamlittle1@hotmail.co.uk>
2020-04-19 18:45:00 +0100

    1.1

---
+++ src/DomainClasses/FoodDomain.java
@@ -0,0 +1,13 @@
+package DomainClasses;
+
+import javafx.collections.ObservableList;
+
+public class FoodDomain {
+
+    private ObservableList<Food>;
+
+    public FoodDomain(){
+        Object fxcollections;
+        Food = fxcollections.ObservaleArrayList;
+    }
+}
